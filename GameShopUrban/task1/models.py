159a31b7437dd7bc1a6ab09cfb97e779bfcfc654
Orland <andrei8orlov@rambler.ru>
2024-10-15 21:32:56 +0300

    Module 19. Task 1. Fix Buyer.name.

--- GameShopUrban/task1/models.py
+++ GameShopUrban/task1/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 
 class Buyer(models.Model):
-    name = models.ForeignKey(User, on_delete=models.CASCADE, to_field='username')
+    name = models.CharField(max_length=150)
     balance = models.DecimalField(decimal_places=2, max_digits=8)
     age = models.IntegerField()
 
